adf1464bfba7ab37c8da449c4124de0d782769d8
Charles Viterbo <charlesviterbo@gmail.com>
2021-12-04 18:49:33 +0100

    Change get_lines
    Remove '\n' in string

--- utils_func/file.py
+++ utils_func/file.py
@@ -3,6 +3,6 @@ import os
 def get_lines(file_path):
     if os.path.exists(file_path):
         file = open(file_path, 'r')
-        lines = file.readlines()
+        lines = file.read().splitlines()
         return lines
     return []
