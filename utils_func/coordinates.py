6dfab09550dd983a3e8df35f9bbe7a5cf6d63f36
Charles Viterbo <charlesviterbo@gmail.com>
2021-12-05 18:26:44 +0100

    Add class coordinates to utils

---
+++ utils_func/coordinates.py
@@ -0,0 +1,13 @@
+class Coordinates:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, other):
+        return (self.x, self.y) == (other.x, other.y)
+
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
